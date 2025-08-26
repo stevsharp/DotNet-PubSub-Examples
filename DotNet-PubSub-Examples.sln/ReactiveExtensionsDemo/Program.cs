9eb5b23481835fc775187208073d2afd169915a1
Spyros Ponaris <sponaris@gmail.com>
2025-08-26 11:55:55 +0300

    Update Program.cs

--- DotNet-PubSub-Examples.sln/ReactiveExtensionsDemo/Program.cs
+++ DotNet-PubSub-Examples.sln/ReactiveExtensionsDemo/Program.cs
@@ -12,10 +12,11 @@ var subA = bus.Subscribe(
     ex => Console.WriteLine($"Subscriber A Error: {ex.Message}"),
     () => Console.WriteLine("Subscriber A Completed"));
 
-var subB  = bus.Subscribe(
-    order => Console.WriteLine($"Subscriber B: Order {order.OrderId} for {order.CustomerEmail} totaling {order.Total}"),
-    ex => Console.WriteLine($"Subscriber B Error: {ex.Message}"),
-    () => Console.WriteLine("Subscriber B Completed"));
+var subB = bus
+            .Where(o => o.Total >= 100)
+            .Select(o => new { o.OrderId, Vat = o.Total * 0.24m })
+            .Subscribe(x =>
+                Console.WriteLine($"[Analytics] Order {x.OrderId}, VAT {x.Vat:F2}"));
 
 bus.OnNext(new OrderSubmitted(Guid.NewGuid(), "oo1@pp.gr" , 150.75m));
 bus.OnNext(new OrderSubmitted(Guid.NewGuid(), "oo2@pp.gr", 10.75m));
